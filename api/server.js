f42eda7620ba156011b1b0d9091ffef5d714650e
Endriyani Rahayu <endriyani.rahayu@shopee.com>
2023-10-24 15:03:56 +0700

    updated api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const auth = require('json-server-auth')
 
 const server = jsonServer.create()
 
@@ -17,9 +16,6 @@ const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
-server.db = router.db
-
-server.use(auth)
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
